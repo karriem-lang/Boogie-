b5290b4f5b088c2cf89f5f59243c12657d5fa6a4
karriem-lang <karriemlang@proton.me>
2025-02-26 16:01:25 -0500

    Initial Commit

---
+++ Boogie!/Boogie_App.swift
@@ -0,0 +1,17 @@
+//
+//  Boogie_App.swift
+//  Boogie!
+//
+//  Created by Karriem Lang on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Boogie_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
